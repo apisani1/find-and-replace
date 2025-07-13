a2dfc473a72e33842b12d2b0e19931e698866d60
Antonio Pisani <antonio.pisani@gmail.com>
2025-07-13 14:42:09 -0400

    release 0.1.1: fix(patch)
    Changes
    --------------------------------------------------------------------------------
    - Add full display of modifications in dry run mode
    - Update documentation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # Changelog
 
+## [0.1.1] - 2025-07-13
+
+ ### Changes
+- Add full display of modifications in dry run mode
+- Update documentation
+
+
+
 ## [0.1.0] - 2025-07-12
 
  ### Initial Release
--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ from datetime import datetime
 project = "find-and-replace"
 copyright = f"{datetime.now().year}, Antonio Pisani"
 author = "Antonio Pisani"
-release = "0.1.0"
+release = "0.1.1"
 
 os.environ["SPHINX_BUILD"] = "True"
 
